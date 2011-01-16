7c378937a5bb27493bd77fe9638d57f484736eb3
Bernards.Gulbis <Bernards.Gulbis@gmail.com>
2011-01-16 15:59:24 +0000

    completed module2

--- SEF/src/sef/module2/sample/HelloWorld.java
+++ SEF/src/sef/module2/sample/HelloWorld.java
@@ -1,7 +1,7 @@
 package sef.module2.sample;
 
 public class HelloWorld {
-	
+
 	public static void main(String arg[]){
 		System.out.println("Hello World!");
 	}
---
+++ SEF/src/sef/module2/sample/SampleClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sef.module2.sample;
+
+/**
+ * @author Bernars Guldis
+ *
+ */
+public class SampleClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is your class");
+
+	}
+
+}
